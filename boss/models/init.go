0bb777213be2a2b4086b595aad051400928f7b1c
kongyuebin <kongyuebin@DESKTOP-1VL48PA.localdomain>
2021-11-11 22:00:24 +0800

    合并一些变量

--- boss/models/init.go
+++ boss/models/init.go
@@ -10,7 +10,6 @@
 package models
 
 import (
-	"boss/conf"
 	"boss/models/accounts"
 	"boss/models/agent"
 	"boss/models/merchant"
@@ -23,22 +22,23 @@ import (
 	"fmt"
 	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/core/logs"
+	"github.com/beego/beego/v2/server/web"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	dbHost := conf.DB_HOST
-	dbUser := conf.DB_USER
-	dbPassword := conf.DB_PASSWORD
-	dbBase := conf.DB_BASE
-	dbPort := conf.DB_PORT
+	dbHost, _ := web.AppConfig.String("mysql::dbhost")
+	dbUser, _ := web.AppConfig.String("mysql::dbuser")
+	dbPassword, _ := web.AppConfig.String("mysql::dbpasswd")
+	dbBase, _ := web.AppConfig.String("mysql::dbbase")
+	dbPort, _ := web.AppConfig.String("mysql::dbport")
 
 	link := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", dbUser, dbPassword, dbHost, dbPort, dbBase)
 
 	logs.Info("mysql init.....", link)
 
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-	orm.RegisterDataBase("default", "mysql", link)
+	_ = orm.RegisterDriver("mysql", orm.DRMySQL)
+	_ = orm.RegisterDataBase("default", "mysql", link)
 	orm.RegisterModel(new(user.UserInfo), new(system.MenuInfo), new(system.SecondMenuInfo),
 		new(system.PowerInfo), new(system.RoleInfo), new(system.BankCardInfo), new(road.RoadInfo),
 		new(road.RoadPoolInfo), new(agent.AgentInfo), new(merchant.MerchantInfo), new(merchant.MerchantDeployInfo),
