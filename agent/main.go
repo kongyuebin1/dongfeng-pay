cc551401033d1008b064b39295474665d9e395c0
kongyuebin <kongyuebin@DESKTOP-1VL48PA.localdomain>
2021-11-18 13:51:06 +0800

    代理后台beego升级到v2.0版本

--- agent/main.go
+++ agent/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	_ "agent/models"
 	_ "agent/routers"
-	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web"
 )
 
 func main() {
-	beego.BConfig.WebConfig.Session.SessionOn = true
-	beego.Run()
+	web.BConfig.WebConfig.Session.SessionOn = true
+	web.Run()
 }
 
